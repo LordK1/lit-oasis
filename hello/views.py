9310723cb5462410fba2c3e59a6207363b0de4d0
K1 <Lord.K1.2009@gmail.com>
2014-11-16 10:22:22 +0330

    the greeting message updated

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import requests
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+	    return HttpResponse('Hello Heroku Django - lit Oasis , K1 !!! :D')
 
 
 def db(request):

7afefae1c5f47e2bfe03b9aa0ba703fb2807b677
K1 <Lord.K1.2009@gmail.com>
2014-11-24 10:57:49 +0330

    Changed article models, added schemamigration

---
+++ article/migrations/0004_initial.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Article'
+        db.create_table(u'article_article', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=40)),
+            ('content', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('publish_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('thumbnail', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+        ))
+        db.send_create_signal(u'article', ['Article'])
+
+        # Adding model 'Comment'
+        db.create_table(u'article_comment', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('body', self.gf('django.db.models.fields.TextField')()),
+            ('publish_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('article', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['article.Article'])),
+        ))
+        db.send_create_signal(u'article', ['Comment'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Article'
+        db.delete_table(u'article_article')
+
+        # Deleting model 'Comment'
+        db.delete_table(u'article_comment')
+
+
+    models = {
+        u'article.article': {
+            'Meta': {'object_name': 'Article'},
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'thumbnail': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '40'})
+        },
+        u'article.comment': {
+            'Meta': {'object_name': 'Comment'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['article.Article']"}),
+            'body': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['article']
